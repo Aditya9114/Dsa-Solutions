ef084976c535db1888d2f521d5a8796ff46c17f5
Aditya9114 <dhomneaditya2@gmail.com>
2025-12-08 19:44:41 +0530

    Time: 31 ms (15.49%) | Memory: 38.2 MB (34.88%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,61 +1,21 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<int> maxheap;
-        if (arr.size() == 1)
-            return arr;
-        priority_queue<int, vector<int>, greater<int>> minheap;
-        vector<int> ans(k, -1);
-        int index = -1;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == x) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            index = floorIndex(arr, x);
-        }
-        
-        // Handle case where all elements are greater than x
-        if (index >= 0) {
-            for (int i = index; i >= 0; i--) {
-                maxheap.push(arr[i]);
-            }
-        }
-        
-        for (int i = index + 1; i < arr.size(); i++) {
-            minheap.push(arr[i]);
-        }
-        
-        for (int i = 0; i < k; i++) {
-            if (maxheap.empty()) {
-                ans[i] = minheap.top();
-                minheap.pop();
-            } else if (minheap.empty()) {
-                ans[i] = maxheap.top();
+        priority_queue<pair<int,int>> maxheap;
+        for(int i=0;i<arr.size();i++){
+            int diff = abs(arr[i]-x);
+            int num = arr[i];
+            maxheap.push({diff,num});
+            if(maxheap.size()>k){
                 maxheap.pop();
-            } else if (abs(maxheap.top() - x) <= abs(minheap.top() - x)) {
-                ans[i] = maxheap.top();
-                maxheap.pop();
-            } else {
-                ans[i] = minheap.top();
-                minheap.pop();
             }
         }
-        sort(ans.begin(), ans.end());
+        vector<int> ans(k,0);
+        for(int i=k-1;i>=0;i--){
+            ans[i] = maxheap.top().second;
+            maxheap.pop();
+        }
+        sort(ans.begin(),ans.end());
         return ans;
-    }
-    
-    int floorIndex(vector<int>& arr, int x) {
-        auto it = lower_bound(arr.begin(), arr.end(), x);
-
-        if (it != arr.end() && *it == x)
-            return it - arr.begin();
-
-        if (it == arr.begin())
-            return -1;
-
-        return (it - arr.begin()) - 1;
     }
 };
