114933adc6edd09fe7c0a48db4afab3058e4ef53
Aditya9114 <dhomneaditya2@gmail.com>
2025-12-13 20:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (38.73%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -3,23 +3,30 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int changeCount5 = 0;
         int changeCount10 = 0;
-        for(int i=0; i<bills.size(); i++){
-            if(bills[i]==5){
+
+        for (int i = 0; i < bills.size(); i++) {
+
+            if (bills[i] == 5) {
                 changeCount5++;
             }
-            else if(bills[i]==10){
-                changeCount10++;
-                if(changeCount5 == 0)return false;
-                changeCount5--;
-            }
-            else if(bills[i]==20){
-                if((changeCount5 < 3  && changeCount10 == 0) || (changeCount5 == 0 && changeCount10 >0))return false;
 
-                else if(changeCount10 <= 0)changeCount5-=3;
-                else{
+            else if (bills[i] == 10) {
+                if (changeCount5 == 0) return false;
+                changeCount5--;
+                changeCount10++;
+            }
+
+            else { 
+                if (changeCount10 > 0 && changeCount5 > 0) {
+                    changeCount10--;
                     changeCount5--;
-                changeCount10--;
-                }     
+                }
+                else if (changeCount5 >= 3) {
+                    changeCount5 -= 3;
+                }
+                else {
+                    return false;
+                }
             }
         }
         return true;
