93623259912887dabef373ebb8ab1264a48470b8
Aditya9114 <dhomneaditya2@gmail.com>
2026-03-27 21:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (98.76%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -7,7 +7,6 @@ public:
 
   
         for(int i = 0; i < mat.size(); i++){
-            int repeat = -1;
        
             for(int j = 0; j < n; j++){
                 // even: check left shift, odd: check right shift
