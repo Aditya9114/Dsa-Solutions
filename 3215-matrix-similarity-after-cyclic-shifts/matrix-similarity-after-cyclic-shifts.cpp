fe100cb1e8f0bab216494050170e77787416ad17
Aditya9114 <dhomneaditya2@gmail.com>
2026-03-27 21:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (67.70%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat[0].size();
+        k = k % n;
+        if(k == 0) return true;
+
+        unordered_map<int,int> mpp;
+        for(int i = 0; i < mat.size(); i++){
+            int repeat = -1;
+            mpp.clear();
+            for(int j = 0; j < n; j++){
+                // even: check left shift, odd: check right shift
+                int shifted = (i % 2 == 0) ? (j + k) % n : (j - k + n) % n;
+                if(mat[i][j] != mat[i][shifted]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
