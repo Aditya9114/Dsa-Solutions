57e486fcaef65d0566062b5bdfc52991b8daf161
Aditya9114 <dhomneaditya2@gmail.com>
2026-05-16 20:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (90.00%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size()-1;
-        int ans = INT_MAX;
-        while(low<=high){
-            int mid = (low+high)/2;
-            int oldMid = mid;
-            if(nums[low] == nums[mid]){
-                while(nums[low] == nums[mid]){
-                    if(low == mid || low>mid)break;
-                    low++;
-                    mid--;
-                }
-                ans = min(ans,nums[low]);
-                low = oldMid + 1;
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                // Min is in right half (excluding mid)
+                low = mid + 1;
+            } 
+            else if (nums[mid] < nums[high]) {
+                // Mid could be the min, so keep it
+                high = mid;
+            } 
+            else {
+                // nums[mid] == nums[high]: can't tell which side
+                // Safe to shrink high by 1 (we still have mid as a candidate)
+                high--;
             }
-            else if(nums[high] == nums[mid]){
-                while(nums[high] == nums[mid]){
-                    if(high == mid || mid>high)break;
-                    high--;
-                    mid++;
-                }
-                ans = min(ans,nums[mid]);
-                high = oldMid - 1;
-            }
-            else if(nums[low] <= nums[mid]){
-                ans = min(ans,nums[low]);
-                low = mid+1;
-            }
-            else{
-                ans = min(ans,nums[mid]);
-                high = mid-1;
-            }
-            ans = min(ans,nums[mid]);
         }
-        return ans;
+
+        return nums[low];
     }
 };
