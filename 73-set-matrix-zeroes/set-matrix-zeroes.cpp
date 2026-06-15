53bc68341d23b39edd34ddcd21f6391365310e95
Aditya Dhomne <dhomneaditya2@gmail.com>
2026-06-15 14:08:31 +0530

    Time: 50 ms (5.29%) | Memory: 24.2 MB (5.23%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        map<pair<int,int>, int> mpp;
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size();j++){
+                if(matrix[i][j] == 0){
+                    mpp[{i,j}] = 1;
+                }
+            }
+        }
+        for(auto x : mpp){
+            int row = x.first.first;
+            int col = x.first.second;
+            for(int i=0; i<matrix[0].size(); i++){
+                matrix[row][i] = 0;
+            }
+            for(int j=0; j<matrix.size();j++){
+                matrix[j][col] = 0;
+            }
+        }
+    }
+};
