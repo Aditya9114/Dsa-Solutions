f281c5d7ab7aa716b978a9b118aee9944fde1810
Aditya Dhomne <dhomneaditya2@gmail.com>
2026-09-22 22:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (40.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int findSum(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp){
+    int findSumRecursive(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp){
         if(row==0 && col==0)return grid[0][0];
         if(row < 0 || col<0)return INT_MAX;
         if(dp[row][col] != -1)return dp[row][col];
-        int up = findSum(grid,row-1,col,dp);
-        int left = findSum(grid,row,col-1,dp);
+        int up = findSumRecursive(grid,row-1,col,dp);
+        int left = findSumRecursive(grid,row,col-1,dp);
         return dp[row][col] = min(left,up)+ grid[row][col];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-       return findSum(grid,grid.size()-1, grid[0].size()-1, dp); 
+        dp[0][0] = grid[0][0];
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                int right=INT_MAX,down=INT_MAX;
+                if(j==0 && i==0)continue;
+                if(i>0){
+                    down = dp[i-1][j];
+                }
+                if(j>0){
+                    right = dp[i][j-1];
+                }
+                dp[i][j] = min(right,down) + grid[i][j];
+            }
+        }
+        return dp[grid.size()-1][grid[0].size()-1];
     }
 };
